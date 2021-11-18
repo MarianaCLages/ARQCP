59997193a7583bbce9e810ffbb64003b86f5014f
Mariana <1200902@isep.ipp.pt>
2021-11-18 15:03:12 +0000

    Ex09 Modulo2

---
+++ modulo2/ex07/1200902/cross_sum.h
@@ -0,0 +1,4 @@
+#ifndef CROSS_SUM_H
+#define CROSS_SUM_H
+short cross_sum(void);
+#endif
---
+++ modulo2/ex09/1200902/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "sum_and_subtract.h"
+
+int main(void) {
+	long res = sum_and_subtract();
+	
+	printf("Sum and subtract: %ld\n", res);
+	
+	return 0;
+}

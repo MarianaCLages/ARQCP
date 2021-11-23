b68f12af33b8dca62ed0580e8b3b87ea635c0d5b
Mariana <1200902@isep.ipp.pt>
2021-11-23 00:16:08 +0000

    ExAnoPassado - Parte C

--- ExAnoPassado/main.c
+++ ExAnoPassado/main.c
@@ -4,4 +4,12 @@
 int main(void) {
 	int num = 6;
 	int dados[12] = {-1,1, 2,-2, 0,0, 5,5, 4,4, 6,7};
+	
+	int *ptr = dados;
+	
+	long long result = total_risco(ptr, num);
+	
+	printf("Resultado dos riscos: ", result);
+	
+	return 0;
 }
--- ExAnoPassado/total_risco.c
+++ ExAnoPassado/total_risco.c
@@ -1,5 +1,32 @@
 #include "result.h"
 
 long long total_risco(int *vec, int num) {
+	int count = 0;
+	int verify = -1; // variável auxiliar para verificar se existem 3 medições válidas consecutivas
 	
+	long long sum = 0;
+	
+	for(int i = 0; i < (num * 2); i++) { // ciclo é feito em pares de valores
+		tempo = *(vec + i);
+		i++; // para passar ao próximo par
+		proximidade = *(vec + i);
+		
+		long long value = result();
+		
+		if(value != 0) {
+			count++;
+			if (count == 3) {
+				verify = 1;
+			}
+		} else {
+			count = 0;
+		}
+		sum = sum + value;
+	}
+	
+	if(verify == 1) {
+		return sum;
+	} else {
+		return -1;
+	}
 }

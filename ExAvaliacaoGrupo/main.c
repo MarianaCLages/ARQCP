30a2e7658968de94e192a52f1a93af8f149713b9
Mariana <1200902@isep.ipp.pt>
2021-11-25 14:49:49 +0000

    ExAvaliacao

--- ExAvaliacaoGrupo/main.c
+++ ExAvaliacaoGrupo/main.c
@@ -4,10 +4,22 @@
 int main(void) {
 	
 	unsigned int pontos[5] = {0x12200005, 0x0710001B, 0x1C09001A, 0x18180018, 0x09010020};
-	
 	int n = sizeof(pontos) / sizeof(int); //Calcular o tamanho do vetor que guarda os pontos
 	
-	unsigned int* apontadorEquipaMaiorPontuacao = vencedor(pontos,n); //Ao chamarmos esta função vamos obter o endereço da equipa com a maior pontuação
+	unsigned int* apontadorEquipaMaiorPontuacao = vencedor(pontos, n); //Ao chamarmos esta função vamos obter o endereço da equipa com a maior pontuação
 	
-	printf("A equipa que teve uma maior pontuação esta guardada no endereço %p\n", apontadorEquipaMaiorPontuacao);
+	printf("A equipa que teve uma maior pontuação está guardada no endereço %p\n", apontadorEquipaMaiorPontuacao);
+	printf("\n");
+	
+	char total_pontuacao_equipa = 0;
+
+	for (char i = 0; i < n*4; i+=4) {
+		total_pontuacao_equipa = *(pontos + i + 1);
+		
+		if(i==0 || i==3) printf("Equpipa %hhd , pontuaçao total : %hhd\n", (i/3 + 1), total_pontuacao_equipa);
+		else printf("Equpipa %hhd , pontuaçao total : %hhd\n", (i/3), total_pontuacao_equipa);
+		
+		
+		
+	}
 }
--- ExAvaliacaoGrupo/vencedor.c
+++ ExAvaliacaoGrupo/vencedor.c
@@ -7,23 +7,23 @@ unsigned char prova3 = 0;
 unsigned int* vencedor(unsigned int *pontos, int n) {
 	
 	unsigned char* ptr = (unsigned char*) pontos; // apontador para ir buscar os pontos de cada prova
-	unsigned int* equipa_maior_pontuacao = pontos;
-	unsigned int* equipa_atual = pontos;
+	unsigned int* equipa_maior_pontuacao = pontos; // apontador que será o output final do programa
+	unsigned int* equipa_atual = pontos; // apontador para ir alterando, quando necessário, para a equipa de maior pontuação
 	
-	unsigned char total_pontuacao = 0;
+	unsigned char total_pontuacao = 0; // char que guarda o valor total da pontuação de uma determinada equipa
 	
 	for(char i = 0; i < n * 4; i += 4) { // variável i anda de 4 em 4 bytes para passar aos resultados da equipa seguinte
 		equipa_atual = pontos + i;
-		prova3 = *(ptr + i); // pontos da prova3 de uma determinada equipa (localizados nos 8 bits menos significativos
+		prova3 = *(ptr + i); // pontos da prova3 de uma determinada equipa (localizados nos 8 bits menos significativos)
 		prova2 = *(ptr + i + 2); // pontos da prova2 de uma determinada equipa (localizados nos 8 bits precedentes aos do total)
 		prova1 = *(ptr + i + 3); // pontos da prova1 de uma determinada equipa (localizados nos 8 bits mais significativos)
 		
-		*(ptr + i + 1) = pontuacao(); // valor do total é proveniente da função realizada em assembly
+		*(ptr + i + 1) = pontuacao(); // chamada da função realizada em assembly para obter o total
 		
-		if(total_pontuacao < *(ptr + i + 1)) {
+		if(total_pontuacao < *(ptr + i + 1)) { // para comparar a maior pontuação de cada equipa
 			total_pontuacao = *(ptr + i + 1);
 			equipa_maior_pontuacao = equipa_atual;
 		}
 	}
-	return equipa_maior_pontuacao;
+	return equipa_maior_pontuacao; // retorna o endereço da equipa com maior pontuação
 }
